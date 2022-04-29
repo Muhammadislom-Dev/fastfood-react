f753c92042b0fb44c1dc8eec59cbab23e663ade3
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-04-29 14:32:30 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import SwiperBox from './components/Swiper/Swiper';
+import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
   return (
     <div className="App">
         <Navbar />
         <SwiperBox />
+        <Sidebar />
     </div>
   );
 }
